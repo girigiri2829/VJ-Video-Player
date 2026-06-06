bb32f0046019e383e6d351cd4f8969825c3d27b6
Sridharan <128954293+girigiri2829@users.noreply.github.com>
2026-06-06 17:06:43 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -1,23 +1,22 @@
-# Don't Remove Credit @VJ_Botz
-# Subscribe YouTube Channel For Amazing Bot @Tech_VJ
-# Ask Doubt on telegram @KingVJ01
 
 
 class script(object):
     START_TXT = """<b>Hello {} 👋,
     
-✏️ I Am Video Player Bot, I Stream File On Website And Earn Unlimited Money 💰.
+✏️ I Am Advance And Powerful Video Player Bot! I Stream File On Website And You Can Earn Unlimited Money 💰
 
-Send Me Any VJDisk Link Or Any File To See My Power.✨
+Send Me Any Disk Link Or Any File To See My Power! ✨
 
 For Uploading File In Quality Option Use Command /quality
 
-More Commands Are /account , /update and /withdraw</b>"""
+More Commands Are /account , /update and /withdraw
+
+Made By - @GI_CARTOONS_OFFICIAL</b>"""
     
     RESTART_TXT = """
-<b>Bᴏᴛ Rᴇsᴛᴀʀᴛᴇᴅ !
+<b>Bot Restarted Successfully!
 
-📅 Dᴀᴛᴇ : <code>{}</code>
-⏰ Tɪᴍᴇ : <code>{}</code>
-🌐 Tɪᴍᴇᴢᴏɴᴇ : <code>Asia/Kolkata</code>
-🛠️ Bᴜɪʟᴅ Sᴛᴀᴛᴜs: <code>v2.7.1 [ Sᴛᴀʙʟᴇ ]</code></b>"""
+Date: <code>{}</code>
+Time: <code>{}</code>
+Timezone: <code>Asia/Kolkata</code>
+Build Status: <code>v2.7.1 [Stable]</code></b>"""
